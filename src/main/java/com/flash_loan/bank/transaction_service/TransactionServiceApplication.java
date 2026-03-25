b0108e00efe1056d72d91f6d45803129d374c067
anden3781 <anderson_samyr@hotmail.com>
2026-03-25 03:24:49 -0500

    openAPI implemented

--- src/main/java/com/flash_loan/bank/transaction_service/TransactionServiceApplication.java
+++ src/main/java/com/flash_loan/bank/transaction_service/TransactionServiceApplication.java
@@ -1,9 +1,12 @@
 package com.flash_loan.bank.transaction_service;
 
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.info.Info;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+@OpenAPIDefinition(info = @Info(title = "Transaction Service API", version = "1.0", description = "Documentation for Transaction Microservice"))
 public class TransactionServiceApplication {
 
 	public static void main(String[] args) {
